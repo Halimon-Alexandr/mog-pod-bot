bfd966144152e67076e3f3a86337a5a2d4b3d247
Halimon-Alexandr <154706310+Halimon-Alexandr@users.noreply.github.com>
2024-05-02 14:08:53 +0300

    Added main files for Telegram bot.

---
+++ mog_pod_bot/main.py
@@ -0,0 +1,181 @@
+from decouple import config
+import modules
+import os
+import pickle
+import pytz
+import telebot
+from telebot import types
+from datetime import datetime, timedelta
+import locale
+
+locale.setlocale(locale.LC_ALL, ("uk_UA", "UTF-8"))
+
+admin = config("TG_CHAT_ADMIN")
+token = config("TG_BOT_TOKEN")
+bot = telebot.TeleBot(token)
+kiev_timezone = pytz.timezone("Europe/Kiev")
+
+
+def help_menu():
+    help_message = "🤖 Коротенька допомога по кнопкам бота:\n\n"
+    help_message += "🔎 /start - Початок роботи з ботом та головне меню.\n"
+    help_message += "🔍 Пошук - Здійснення пошуку розкладу автобусів за назвою зупинки. Можна писати не повну назву\n"
+    help_message += "📖 Список - Перегляд списку всіх доступних зупинок.\n"
+    help_message += "🔼 Попередній - Перехід до попереднього результата пошуку.\n"
+    help_message += (
+        "▶️ Оновити - Оновлення результатів пошуку відносно поточного часу.\n"
+    )
+    help_message += "🔽 Наступний - Перехід до наступного результату пошуку.\n"
+    help_message += "👉Основне меню👈 - Повернення до головного меню.\n"
+    help_message += "💬 Допомога - Повна інформація про користування ботом.\n"
+    return help_message
+
+
+def generate_keyboard(button_texts):
+    markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
+    rows = [button_texts[i : i + 3] for i in range(0, len(button_texts), 3)]
+    for row in rows:
+        markup.add(*[types.KeyboardButton(text) for text in row])
+    return markup
+
+
+# Обробник команди /start
+@bot.message_handler(commands=["start"])
+def start(message):
+    user_name = (
+        message.from_user.first_name if message.from_user.first_name else "друже"
+    )
+    button_texts = ["Пошук 🔎", "Список 📖", "Допомога 🙋"]
+    markup = generate_keyboard(button_texts)
+    bot.send_message(
+        message.chat.id,
+        f"Привіт, {user_name}! 👋 \nЯ бот для  пошуку графіку руху міських автобусів  в місті Могилів-Подільський",
+        reply_markup=markup,
+    )
+
+
+# Визначення обробника текстових повідомлень
+@bot.message_handler(content_types=["text"])
+def func(message):
+    # Перевірка, чи є користувач в користуватському словнику
+    if message.chat.id not in user_data:
+        username = message.from_user.first_name
+        if message.from_user.last_name is not None:
+            username += f" {message.from_user.last_name}"
+        bot.send_message(admin, f"Ура-а-а! У нас новий користувач {username}.")
+        user_data[message.chat.id] = Searcher(username=username)
+    user_data[message.chat.id].last_visit = datetime.now(kiev_timezone)
+
+    # Обробка певних текстових повідомлень
+    if message.text == "Звіт":
+        current_time = datetime.now(kiev_timezone)
+        sorted_user_list = sorted(
+            user_data.items(), key=lambda item: item[1].last_visit, reverse=True
+        )[:10]
+        sum_interaction_count = sum(
+            map(lambda item: item[1].interaction_count, user_data.items())
+        )
+        for user_id, user_Searcher in sorted_user_list:
+            if user_Searcher.last_visit.date() == current_time.date():
+                formatted_time = (
+                    f"сьогодні о {user_Searcher.last_visit.strftime('%H:%M')}"
+                )
+            elif user_Searcher.last_visit.date() == current_time.date() - timedelta(
+                days=1
+            ):
+                formatted_time = f"вчора о {user_Searcher.last_visit.strftime('%H:%M')}"
+            else:
+                formatted_time = f"{user_Searcher.last_visit.strftime('%d %B')}, о {user_Searcher.last_visit.strftime('%H:%M')}"
+
+            bot.send_message(
+                message.chat.id,
+                f"{user_Searcher.username},\nЗапит : {user_Searcher.search_query}.\nДата : {formatted_time}",
+            )
+        bot.send_message(
+            message.chat.id,
+            f"Всього користувачів {len(user_data)}, загальна кількість запитів від останнього перезавантаження {sum_interaction_count}",
+        )
+
+    elif message.text == "Список 📖":
+        message.text, button_texts = user_data[message.chat.id].find(bus_stop_name="")
+        markup = generate_keyboard(button_texts)
+        bot.send_message(
+            message.chat.id, "Оберіть потрібну зупинку із списку!", reply_markup=markup
+        )
+
+    elif message.text == "Пошук 🔎":
+        bot.send_message(
+            message.chat.id,
+            "Введіть назву зупинки! \nНе обов'язково писати повну назву",
+        )
+
+    elif message.text == "Допомога 🙋":
+        bot.send_message(message.chat.id, help_menu())
+
+    elif message.text == "🔼 Попередній":
+        user_data[message.chat.id].offset -= 1
+        message.text, button_texts = user_data[message.chat.id].find()
+        markup = generate_keyboard(button_texts)
+        bot.send_message(message.chat.id, message.text, reply_markup=markup)
+
+    elif message.text == "▶\nОновити":
+        user_data[message.chat.id].offset = 0
+        message.text, button_texts = user_data[message.chat.id].find()
+        markup = generate_keyboard(button_texts)
+        bot.send_message(message.chat.id, message.text, reply_markup=markup)
+
+    elif message.text == "🔽 Наступний":
+        user_data[message.chat.id].offset += 1
+        message.text, button_texts = user_data[message.chat.id].find()
+        markup = generate_keyboard(button_texts)
+        bot.send_message(message.chat.id, message.text, reply_markup=markup)
+
+    elif message.text == "👉Основне меню👈":
+        button_texts = ["Пошук 🔎", "Список 📖", "Допомога 🙋"]
+        markup = generate_keyboard(button_texts)
+        bot.send_message(
+            message.chat.id, "Ви повернулись до головного меню!", reply_markup=markup
+        )
+    else:
+        # Якщо повідомлення не було перехоплено, виконується пошук автобусної зупинки
+        user_data[message.chat.id].offset = 0
+        message.text, buttons_texts = user_data[message.chat.id].find(
+            bus_stop_name=message.text
+        )
+        markup = generate_keyboard(buttons_texts)
+        bot.send_message(message.chat.id, message.text, reply_markup=markup)
+
+    # Збереження користувацьких даних в файл
+    with open("user_data.pickle", "wb") as f:
+        pickle.dump(user_data, f)
+
+
+bot.send_message(admin, "Бот перезавантажено!")
+# Відновлення даних з файлу
+if os.path.exists("user_data.pickle"):
+    with open("user_data.pickle", "rb") as file:
+        user_data = pickle.load(file)
+
+    current_time = datetime.now(kiev_timezone)
+    inactive_users = []
+
+    for user_id, user in user_data.items():
+        if (current_time - user.last_visit).days > 30:
+            inactive_users.append(user_id)
+
+    for user_id in inactive_users:
+        del user_data[user_id]
+
+    with open("user_data.pickle", "wb") as file:
+        pickle.dump(user_data, file)
+
+    if inactive_users:
+        deleted_users_message = "Видалені користувачі:\n"
+        for user_id in inactive_users:
+            deleted_users_message += f"- Користувач з ID {user_id}\n"
+        bot.send_message(admin, deleted_users_message)
+else:
+    user_data = {}
+
+# Processing other commands and text messages
+bot.polling(none_stop=True)
---
+++ mog_pod_bot/modules/searcher.py
@@ -0,0 +1,158 @@
+from modules.schedule import workday, weekend, bus_arrival_times
+import pytz
+from datetime import datetime, date, timedelta
+
+kiev_timezone = pytz.timezone("Europe/Kiev")
+
+
+class Searcher:
+    """
+    This class includes methods for navigating through the bus schedule ('next' and 'previous') and searching for bus information based on the provided parameters ('find').
+    The class also tracks user activity, including the number of interaction_count and the timestamp of the last visit.
+    The bus schedule is determined based on the current day, and the result string is formatted with relevant information about the next scheduled bus.
+    """
+
+    workday_schedule = sorted(workday)
+    weekend_schedule = sorted(weekend)
+    bus_arrival_times = bus_arrival_times
+    interaction_count = 0
+    last_visit = None
+    search_query = ""
+    offset = 0
+
+    def __init__(self, username=None):
+        if username is None:
+            self.username = "Без імені"
+        else:
+            self.username = username
+
+    def bus_schedule_generator(self):
+        """Generate bus schedule information based on the current day of the week (workday or weekend) and the provided bus arrival times."""
+        index = 0
+        skip_stops = {
+            "Ощадбанк",
+            "Монтажний технікум",
+            "Машинобудівний завод",
+            "Школа № 5",
+            "Лікарня",
+            "Верхня Немія",
+            "Залізничний вокзал",
+            "Маслозавод",
+            "Автовокзал",
+            "Школа № 2",
+        }
+        change_bus = True
+        weekday = datetime.now(kiev_timezone).weekday()
+        is_workday = weekday < 5
+        bus_schedule = self.workday_schedule if is_workday else self.weekend_schedule
+
+        while len(bus_schedule) != index:
+            try:
+                current_time = bus_schedule[index]
+
+                for direction, offset in self.bus_arrival_times.get(
+                    self.search_query, {}
+                ).items():
+                    departure_time = bus_schedule[index]
+                    bus_number = (index % 2) + 1
+                    departure_time_dt = datetime.now(kiev_timezone).replace(
+                        hour=int(departure_time[:2]),
+                        minute=int(departure_time[3:]),
+                        second=0,
+                    )
+                    arrival_time = departure_time_dt + timedelta(minutes=offset)
+                    if self.search_query in skip_stops and change_bus:
+                        index += 1
+                        change_bus = False
+
+                    yield direction, bus_number, arrival_time
+                index += 1
+                change_bus = True
+            except IndexError:
+                break
+
+    def find(self, bus_stop_name=None, current_time=None):
+        """
+        Search for bus information based on the provided bus stop name and current time.
+
+        Args:
+        - bus_stop_name (str): The name of the bus stop to search for. If not provided, the last searched bus stop will be used.
+        - current_time (datetime): The current time to use for the search. If not provided, the current system time will be used.
+
+        Returns:
+        - str: A formatted string containing information about the next scheduled bus arrival at the specified bus stop.
+        - types.ReplyKeyboardbuttons_texts: A keyboard buttons_texts with navigation buttons for interacting with the bot.
+        """
+        # Update the last visit timestamp
+        self.last_visit = datetime.now(kiev_timezone)
+        # Updating the number of interaction_count
+        self.interaction_count += 1
+        # Set default values for bus_stop_name and current_time if not provided
+        if current_time is None:
+            current_time = self.last_visit
+        if bus_stop_name is None:
+            bus_stop_name = self.search_query
+        # Iterate through the base schedule to find the requested bus stop
+        found_stops = []
+        for current_bus_stop, _ in self.bus_arrival_times.items():
+            if bus_stop_name.lower() in current_bus_stop.lower():
+                found_stops.append(current_bus_stop)
+        if len(found_stops) == 1:
+            self.search_query = found_stops[0]
+            result = f"🚏 {found_stops[0]}."
+        elif len(found_stops) > 1:
+            buttons_texts = found_stops
+            return (
+                "За вашим  запитом знайдено декілька зупинок, \nбудь ласка, оберіть потрібну з нижче наведених",
+                buttons_texts,
+            )
+        else:
+            buttons_texts = ["Пошук 🔎", "Список 📖", "Допомога 🙋"]
+            return (
+                "Вибачте, за вашим запитом не знайдено жодної із зупинок. \nБудь ласка, перевірте правильність написання назви або спробуйте знайти потрібне, нажавши кнопку  'Список 📖'",
+                buttons_texts,
+            )
+        schedule_current_busstop = list(self.bus_schedule_generator())
+        schedule_current_busstop = sorted(
+            schedule_current_busstop, key=lambda item: item[2]
+        )
+        for index, (direction, bus_number, arrival_time) in enumerate(
+            schedule_current_busstop
+        ):
+            if current_time.time() <= arrival_time.time():
+                next_bus_info = schedule_current_busstop[
+                    (index + self.offset) % len(schedule_current_busstop)
+                ]
+                direction = next_bus_info[0]
+                bus_number = next_bus_info[1]
+                arrival_time = next_bus_info[2]
+
+                # Розрахунок часу залишку
+                time_left = arrival_time - datetime.now(kiev_timezone)
+                hours_left = time_left.seconds // 3600
+                minutes_left = (time_left.seconds % 3600) // 60
+
+                # Додання інформації про час залишку до результату
+                result += f"\n👉 В напрямку {direction}"
+                result += f"\n🚌 № {bus_number}, ⌚ о {arrival_time.strftime('%H:%M')}"
+                result += (
+                    f"\nЧерез {hours_left} годин {minutes_left} хвилин\n"
+                    if hours_left
+                    else f"\nЧерез {minutes_left} хвилин\n"
+                )
+                break
+
+            # If no bus information was added, perform a search starting from midnight
+        if result == f"🚏 {self.search_query}.":
+            return self.find(
+                current_time=datetime.now(kiev_timezone).replace(
+                    hour=0, minute=0, second=0
+                )
+            )
+        buttons_texts = [
+            "🔼 Попередній",
+            "▶\nОновити",
+            "🔽 Наступний",
+            "👉Основне меню👈",
+        ]
+        return result, buttons_texts
